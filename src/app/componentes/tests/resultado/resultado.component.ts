859d2f211178b78a73228b6bf34a38d1f3ea1cc5
Paty G <patyg@MacBook-Pro-de-Paty.local>
2023-05-06 23:16:01 -0600

    Resultados del test y estilo de articulos

--- src/app/componentes/tests/resultado/resultado.component.ts
+++ src/app/componentes/tests/resultado/resultado.component.ts
@@ -9,7 +9,7 @@ import { trigger, transition, style, animate } from '@angular/animations';
     trigger('fadeIn', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('2s', style({ opacity: 1 })),
+        animate('2.5s', style({ opacity: 1 })),
       ]),
     ]),
   ],
@@ -21,34 +21,34 @@ export class ResultadoComponent implements OnInit {
     {
       title: 'Baja',
       descripcion:
-        'Los resultados indican una probabilidad baja de TDAH. Aun así, si siguen preocupados, no duden en consultar con un profesional de la salud calificado para una evaluación más exhaustiva. Juntos, pueden abordar cualquier inquietud que puedan tener.',
+        'Gracias por tomarse el tiempo para completar el test en nombre de su hijo. Los resultados indican una probabilidad baja de TDAH. Aun así, si siguen preocupados, les recomendamos que consulten con un profesional de la salud calificado para una evaluación más exhaustiva. Los invitamos a ir a la sección contactos.',
     },
     {
       title: 'Moderada',
       descripcion:
-        'Según los resultados del test, existe una probabilidad moderada de que su hijo tenga TDAH. No se preocupen, sería beneficioso buscar la opinión de un profesional de la salud calificado para una evaluación más detallada. Con el apoyo adecuado, su hijo puede superar los desafíos que el TDAH pueda presentar.',
+        'Según los resultados del test, existe una probabilidad moderada de que su hijo tenga TDAH. Sería beneficioso buscar la opinión de un profesional de la salud calificado para una evaluación más detallada y abordar las inquietudes que puedan tener. Pueden ir a la sección contactos ahí encontraran a especialistas en el area.',
     },
     {
       title: 'Alta',
       descripcion:
-        'Los resultados del test sugieren una probabilidad alta de TDAH en su hijo. Les recomendamos encarecidamente que consulten con un profesional de la salud calificado para obtener una evaluación adecuada y un diagnóstico preciso. Queremos que sepan que, con el apoyo y las estrategias adecuadas, los niños con TDAH pueden desarrollarse exitosamente y disfrutar de una vida plena. Juntos, pueden enfrentar este desafío y ayudar a su hijo a alcanzar su máximo potencial.',
+        'Los resultados del test sugieren una probabilidad alta de TDAH en su hijo. Les recomendamos encarecidamente que consulten con un profesional de la salud calificado para obtener una evaluación adecuada y un diagnóstico preciso. En la sección Contactos pueden visualizar especialistas y centros expertos en el area. Sepan que con el apoyo y las estrategias adecuadas, los niños con TDAH pueden desarrollarse exitosamente y disfrutar de una vida plena.',
     },
   ];
   resultadosAdulto = [
     {
       title: 'Baja',
       descripcion:
-        'Los resultados indican una probabilidad baja de TDAH. Si aún tienes preocupaciones, no dudes en consultar a un profesional de la salud calificado para una evaluación más completa. Siempre es un buen momento para abordar cualquier inquietud que puedas tener y buscar el apoyo necesario.',
+        'Los resultados indican una probabilidad baja de TDAH. Si aún tienes preocupaciones, no dudes en consultar a un profesional de la salud calificado para una evaluación más completa. Siempre es un buen momento para abordar cualquier inquietud que puedas tener y buscar el apoyo necesario. Te invitamos a visitar la sección de contactos donde encontrarás información de especialistas y centros especializados en el área.',
     },
     {
       title: 'Moderada',
       descripcion:
-        'Los resultados del test sugieren una probabilidad alta de TDAH. Te recomendamos encarecidamente que consultes con un profesional de la salud calificado para obtener una evaluación adecuada y un diagnóstico preciso. Queremos que sepas que, con el apoyo y las estrategias adecuadas, puedes superar los desafíos del TDAH y alcanzar tus metas en la vida. Este puede ser el comienzo de una nueva etapa llena de oportunidades y crecimiento personal.',
+        'Según los resultados del test, existe una probabilidad moderada de TDAH. No te preocupes, este puede ser el comienzo de un cambio positivo en tu vida. Te recomendamos buscar la opinión de un profesional de la salud calificado para una evaluación más detallada. Con el apoyo adecuado, podrás enfrentar los desafíos del TDAH y abrirte camino hacia un futuro exitoso y gratificante. Te invitamos a visitar la sección de contactos donde encontrarás información de especialistas y centros especializados en el área.',
     },
     {
       title: 'Alta',
       descripcion:
-        'Los resultados del test sugieren una probabilidad alta de TDAH en su hijo. Les recomendamos encarecidamente que consulten con un profesional de la salud calificado para obtener una evaluación adecuada y un diagnóstico preciso. Queremos que sepan que, con el apoyo y las estrategias adecuadas, los niños con TDAH pueden desarrollarse exitosamente y disfrutar de una vida plena. Juntos, pueden enfrentar este desafío y ayudar a su hijo a alcanzar su máximo potencial.',
+        'Los resultados del test sugieren una probabilidad alta de TDAH. Te recomendamos encarecidamente que consultes con un profesional de la salud calificado para obtener una evaluación adecuada y un diagnóstico preciso. Queremos que sepas que, con el apoyo y las estrategias adecuadas, puedes superar los desafíos del TDAH y alcanzar tus metas en la vida. Este puede ser el comienzo de una nueva etapa llena de oportunidades y crecimiento personal. Te invitamos a visitar la sección de contactos donde encontrarás información de especialistas y centros especializados en el área.',
     },
   ];
 
